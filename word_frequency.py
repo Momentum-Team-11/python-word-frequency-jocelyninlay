890cfd2b9478cb9c0331a9a5dc10da32135b01a0
Jocelyn <inlay.j@gmail.com>
2022-02-23 17:17:25 -0500

    sorted word_freq

--- word_frequency.py
+++ word_frequency.py
@@ -25,14 +25,22 @@ def print_word_freq(file):
         text_string = text_string.replace("—", "")
         text_string = text_string.lower()
         text_string = text_string.split()
+        no_stop_words = {}
         #print(text_string[0:100])
         #now loop through the words to remove STOP WORDS
-        for words in text_string:
+        for word in text_string:
             # need to check if the words in the file are in STOP_WORDS
-            if words not in STOP_WORDS:
-                print(words)
-
-
+            if word not in STOP_WORDS:
+                if word in no_stop_words:
+                    no_stop_words[word] += 1
+                else:
+                    no_stop_words[word] = 1
+        #print(no_stop_words)
+        word_freq = []
+        for key, value in no_stop_words.items():
+            word_freq.append((value, key))
+        word_freq.sort(reverse=True)
+        print(word_freq)
 # delete punctutation
 # deinfitely gonna need a for loop .. for lines in file??
 # want to use x.lower() to make everything lowercase
